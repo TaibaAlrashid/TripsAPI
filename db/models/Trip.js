69bf53226930faa5a9f78796b8c2c53c5db95315
alaalhumaidan <alaalhumaidan@gmail.com>
2021-07-29 17:20:43 +0300

    BERegister

--- db/models/Trip.js
+++ db/models/Trip.js
@@ -1,25 +1,22 @@
-
 module.exports = (sequelize, DataTypes) => {
-    const Trip = sequelize.define("Trip", {
+  const Trip = sequelize.define("Trip", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  });
 
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        slug: {
-            type: DataTypes.STRING,
-            unique: true
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        image: {
-            type: DataTypes.STRING,
-        },
-    });
-
-    // SequelizeSlugify.slugifyModel(Trip, { source: ["title"] });
-    return Trip;
-
+  // SequelizeSlugify.slugifyModel(Trip, { source: ["title"] });
+  return Trip;
 };

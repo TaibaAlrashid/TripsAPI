b12a589513f129ba5443e810058c82cea2fb5758
Maha Almutairi <carsridder10@gmail.com>
2021-08-03 15:27:22 +0300

    Update

--- app.js
+++ app.js
@@ -9,15 +9,12 @@ const passport = require("passport");
 const { localStrategy } = require("./API/middleware/passport");
 const { jwtStrategy } = require("./API/middleware/passport");
 
-
 // ****************** ROUTES IMPORT ******************
 const userRoutes = require("./API/user/routes");
 const triproutes = require("./trips/routes");
 
-
-
 // Request ----> Middleware ------> Next() ------> Router/Controller
-//         <-------------------------------------- Response  
+//         <-------------------------------------- Response
 app.use(cors());
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -29,30 +26,27 @@ app.use("/trips", triproutes);
 app.use(userRoutes);
 app.use("/media", express.static("media"));
 
-
 // ****************** Handling Errors ******************
 app.use((err, req, res, next) => {
-    res
-        .status(err.status || 500)
-        .json({ message: err.message || "Internal Server Error." });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal Server Error." });
 });
-
 
 // ****************** Handling No found Path ******************
 app.use((req, res, next) => {
-    res.status(404).json({ message: "Path Not Found." });
+  res.status(404).json({ message: "Path Not Found." });
 });
 
-
 const run = async () => {
-    try {
-        await db.sequelize.sync({ alter: true });
-        console.log("Connection to the database successful!");
-        await app.listen(9000, () => {
-            console.log("The application is running on localhost:9000");
-        });
-    } catch (error) {
-        console.error("Error connecting to the database: ", error);
-    }
+  try {
+    await db.sequelize.sync({ alter: true });
+    console.log("Connection to the database successful!");
+    await app.listen(9000, () => {
+      console.log("The application is running on localhost:9000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
 };
 run();
